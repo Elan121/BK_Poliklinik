15f8d37b8e0698c9e7504ccdfa71e2eafd778545
Elan121 <86473501+Elan121@users.noreply.github.com>
2023-12-28 00:34:08 +0700

    Add files via upload

---
+++ dokter_login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin | Log in </title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="index.php" class="h1"><b>Login</b> Admin</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="user/dokter.php" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Nama" id="nama" name="nama">
+          <div class="input-group-append">
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="No.HP" id="nhp" name="nhp">
+          <div class="input-group-append">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="AdminLTE/dist/js/adminlte.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Poliklinik | Login</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition layout-top-nav">
+<div class="wrapper">
+
+	<div class="p-5 bg-primary text-white text-center">
+		<h1>Selamat Datang di Poliklinik</h1>
+		<p>Silahkan login untuk memulai</p> 
+	</div>
+
+  
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+	<div class="content-header">
+    <div class="container-fluid">
+    <div class="row mb-2">
+        <div class="col-sm-6">
+        <h1 class="m-0">Menu Login</h1>
+        </div><!-- /.col -->
+		</div><!-- /.row -->
+		</div><!-- /.container-fluid -->
+	</div>
+    <!-- /.col-md-6 -->
+		<div class='row'>
+        <div class="col-lg-4">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="m-0">Login Admin</h5>
+              </div>
+              <div class="card-body">
+                <h6 class="card-title">Login sebagai Admin</h6>
+
+                <p class="card-text">Gunakan login Admin untuk masuk sebagai Admin</p>
+                <a href="admin_login.php" class="btn btn-primary btn-center">Login</a>
+              </div>
+            </div>
+			
+			
+		</div>
+		<div class="col-lg-4">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="m-0">Login Dokter</h5>
+              </div>
+              <div class="card-body">
+                <h6 class="card-title">Login sebagai Dokter</h6>
+
+                <p class="card-text">Gunakan login Dokter untuk masuk sebagai Dokter</p>
+                <a href="dokter_login.php" class="btn btn-primary btn-center">Login</a>
+              </div>
+            </div>
+			
+			
+		</div>
+		<div class="col-lg-4">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="m-0">Login Pasien</h5>
+              </div>
+              <div class="card-body">
+				<h6 class="card-title">Login sebagai Pasien</h6>
+                <p class="card-text">Gunakan login Pasien untuk masuk sebagai Pasien</p>
+                <a href="#" class="btn btn-primary btn-center">Login</a>
+              </div>
+            </div>
+			
+			
+		</div>
+		
+		</div>
+		</div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="AdminLTE/dist/js/adminlte.min.js"></script>
+</body>
+</html>
