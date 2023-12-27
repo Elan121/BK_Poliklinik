4c34ee722eb38ada235e1cb9f84bea2fc0e7120d
Elan121 <86473501+Elan121@users.noreply.github.com>
2023-12-28 00:33:03 +0700

    Add files via upload

---
+++ user/admin.php
@@ -0,0 +1,36 @@
+<?php
+include '../koneksi.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Ambil nilai dari form
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+
+
+    // Query untuk menambahkan data obat ke dalam tabel
+    $query = "SELECT * From admin WHERE email='$email' and password='$pass'";
+	
+	$result = mysqli_query($mysqli, $query);
+	#$found = $result->num_rows;
+	
+	if ($result->num_rows > 0) {
+        $data = $result->fetch_assoc();
+        $_SESSION['nama'] = $data['nama'];
+		#echo $_SESSION['nama'];
+        echo "<script>alert('Login Success');</script>";
+        echo "<script>location='../admin_dashboard.php';</script>";
+		#header("Location: ../admin_obat.php");
+	} else {
+		echo "<script>alert('User not found');</script>";
+        echo "<script>location='../index.php';</script>";
+	}
+    
+
+   
+}
+
+// Tutup koneksi
+mysqli_close($mysqli);
+
+?>
